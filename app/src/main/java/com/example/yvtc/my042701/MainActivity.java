c5729d4a8a7600d3d5873ab4e6efae5180e8a2f3
twn9320 <twn9320@gmail.com>
2017-04-27 14:09:20 +0800

    FireBaseDatabase(read)

--- app/src/main/java/com/example/yvtc/my042701/MainActivity.java
+++ app/src/main/java/com/example/yvtc/my042701/MainActivity.java
@@ -8,15 +8,21 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-
+    FirebaseDatabase database;
+    DatabaseReference myRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("message");
         Intent it = getIntent();
         String str = it.getStringExtra("msg");
         if(str!=null){
@@ -24,14 +30,25 @@ public class MainActivity extends AppCompatActivity {
             tv.setText(str);
 
         }
-        Toast.makeText(MainActivity.this,str,Toast.LENGTH_SHORT).show();
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                TextView tv2 = (TextView)findViewById(R.id.textView2);
+                String value = dataSnapshot.getValue(String.class);
+                tv2.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
     public void clicksave(View v){
         // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-        EditText ed = (EditText)findViewById(R.id.editText);
 
+        EditText ed = (EditText)findViewById(R.id.editText);
         myRef.setValue(ed.getText().toString());
     }
 }
