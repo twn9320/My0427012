a5e02446cfcf9db52b4de83cad6f8f62ca8c1f09
twn9320 <twn9320@gmail.com>
2017-04-27 13:37:08 +0800

    FireBaseDatabase

--- app/src/main/java/com/example/yvtc/my042701/MainActivity.java
+++ app/src/main/java/com/example/yvtc/my042701/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.yvtc.my042701;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +25,13 @@ public class MainActivity extends AppCompatActivity {
 
         }
         Toast.makeText(MainActivity.this,str,Toast.LENGTH_SHORT).show();
+    }
+    public void clicksave(View v){
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        EditText ed = (EditText)findViewById(R.id.editText);
+
+        myRef.setValue(ed.getText().toString());
     }
 }
